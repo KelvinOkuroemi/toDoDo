bd49fb29b23e2bf25fef125c13c35f1a3b5c5ba6
Kelvin Okuroemi <okuroemidouebikelvin@gmail.com>
2023-04-19 12:14:40 +0100

    Stage beginner files

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// Feature => Make a reset button to clear everything from scratch
+// Feature => Add the customized animated alert function 
+// Feature => Make sections to be able to store 
+
+const hello = "Halloooo"
+alert(hello)
